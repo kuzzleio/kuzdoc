50b9ef353cdc77abb40e85fc78b2e28b4e1023b7
Luca Marchesini <xbill82@gmail.com>
2020-01-10 09:49:48 +0100

    [framework:install] remove `master` in default branch

--- src/commands/framework/install.ts
+++ src/commands/framework/install.ts
@@ -14,7 +14,7 @@ export default class FrameworkInstall extends Command {
     branch: flags.string({
       char: 'b',
       description: 'The framework branch that should be checked out',
-      default: process.env.FRAMEWORK_BRANCH || 'master'
+      default: process.env.FRAMEWORK_BRANCH
     }),
     destination: flags.string({
       char: 'd',
